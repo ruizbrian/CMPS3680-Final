2b28c101c4b0699776ade9ff00f7013d0504e258
ruizbrian <bruiz26@csub.edu>
2024-05-13 23:52:29 -0700

    adjusted drop.php to work with multiple tables

--- Forum/sql/drop.php
+++ Forum/sql/drop.php
@@ -1,18 +1,29 @@
 <?php
 require_once('sqlTools.php');
 
-$query = "DROP TABLES users, posts, comments";
-
-//MODIFIY DATABASE
+// Close existing connections to release locks
 $db = getConnection();
+closeConnection($db);
 
-if(mysqli_query($db, $query)) {
-    echo "FORUM TABLES SUCCESSFULLY REMOVED\n";
+// Disable foreign key checks
+$queryDisableFK = "SET FOREIGN_KEY_CHECKS=0";
+$db = getConnection(); // Reopen connection
+mysqli_query($db, $queryDisableFK);
+
+// Drop tables
+$queryDropTables = "DROP TABLE users, posts, comments";
+
+if(mysqli_query($db, $queryDropTables)) {
+    echo "Forum tables successfully removed\n";
 } else {  
-    echo "FAILED TO REMOVE FORUM TABLES\n";
+    echo "Failed to remove forum tables\n";
     die();
 }
 
-closeConnection($db);
+// Enable foreign key checks
+$queryEnableFK = "SET FOREIGN_KEY_CHECKS=1";
+mysqli_query($db, $queryEnableFK);
 
+// Close connection
+closeConnection($db);
 ?>
