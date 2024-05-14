60b7cc17f1997d3c16f311d07480cff5db905760
ggarrreto <gstoneg616@gmail.com>
2024-05-14 03:32:51 -0700

    update index.php and css

--- Forum/index.php
+++ Forum/index.php
@@ -27,36 +27,16 @@ $mysqli->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forum</title>
     <link rel="stylesheet" href="style.css">
-    <!-- Link Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class="home-page">
-    <!-- Bootstrap Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Forum</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="post.php">Create New Post</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<body>
+    
+<?php include 'navbar.php'; ?>
 
     <div class="container">
+    <?php include 'navbar.php'; ?>
         <h1>Forum</h1>
+        <a href="post.php">Create New Post</a>
         <div class="posts">
             <?php if (!empty($posts)) : ?>
                 <?php foreach ($posts as $post) : ?>
@@ -70,9 +50,6 @@ $mysqli->close();
             <?php endif; ?>
         </div>
     </div>
-
-    <!-- Bootstrap JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
