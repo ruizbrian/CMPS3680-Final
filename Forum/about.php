f0462f0bfa24ad0ea2d194fd7967fbfb909dd3a3
ggarrreto <gstoneg616@gmail.com>
2024-05-14 01:38:38 -0700

    import bootstrap

--- Forum/about.php
+++ Forum/about.php
@@ -9,6 +9,7 @@
 <body>
 
 <?php include 'navbar.php'; ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-..." crossorigin="anonymous">
 
 <div class="container">
     <div class="about-section">
--- Forum/index.php
+++ Forum/index.php
@@ -9,6 +9,7 @@
 <body class="home-page"> <!-- Add the home-page class -->
 
 <?php include 'navbar.php'; ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-..." crossorigin="anonymous">
 
 <div class="container">
     <?php
